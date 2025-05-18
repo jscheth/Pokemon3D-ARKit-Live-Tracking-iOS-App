d5f96a7fcfc5de08053a9be46d5806703e1ef177
jscheth <sovichea.chet@gmail.com>
2025-05-18 15:36:25 -0700

    Initial Commit

---
+++ Pokemon3DTests/Pokemon3DTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon3DTests.swift
+//  Pokemon3DTests
+//
+//  Created by Jonathan Cheth on 5/18/25.
+//
+
+import Testing
+@testable import Pokemon3D
+
+struct Pokemon3DTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
